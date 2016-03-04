acd07a75c6d640aa8ce91b82df18c38533cda431
aaltaev <andalt92@gmail.com>
2016-03-04 19:05:51 +0800

    deleted resources from git

--- resources/js/convert.js
+++
@@ -1,7 +0,0 @@
-$(function () {
-    $("#content").bind("input change", function () {
-        $.post("/convert", {md: $("#content").val()}, function (response) {
-            $("#md_html").html(response.html)
-        })
-    })
-})
